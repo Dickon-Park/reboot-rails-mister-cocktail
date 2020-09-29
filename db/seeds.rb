e3da7db3378aca7618a2c79322bdd94b95a729dc
Dickon Park <dickon.j.park@gmail.com>
2020-09-29 12:30:53 +0200

    mressage

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+ingredients_url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ ingredients_serialized = open(ingredients_url).read
+puts "---"
+ ingredients_hash = JSON.parse(ingredients_serialized)
+
+ingredients_array = ingredients_hash["drinks"]
+
+ingredients_array.each do |ingredient|
+  ingredient_name = ingredient["strIngredient1"]
+  p Ingredient.create(name: ingredient_name)
+end
